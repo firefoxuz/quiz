09bf8ef9c5fe589fcc1817127b0a87832fd949c3
Sultonov Daler <daler.sultonov.00@gmail.com>
2022-04-08 14:00:10 +0500

    Some route's name changes

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'sendFaceData'])->name('login');
-Route::post('/login/face', [App\Http\Controllers\Api\Auth\LoginController::class, 'authenticate'])->name('login');
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'sendFaceData']);
+Route::post('/login/face', [App\Http\Controllers\Api\Auth\LoginController::class, 'authenticate']);
 Route::post('/register', [App\Http\Controllers\Api\Auth\RegisterController::class, 'register']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::group(['prefix' => '/user'], function () {
         Route::get('/', [App\Http\Controllers\Api\UserController::class, 'index']);
         Route::put('/', [App\Http\Controllers\Api\UserController::class, 'update']);
     });
-    Route::apiResource('/quizzes', App\Http\Controllers\Api\Quiz\QuizController::class, ['only' => ['index', 'show']]);
-    Route::apiResource('/takes', App\Http\Controllers\Api\Quiz\TakeController::class, ['only' => ['index', 'show']]);
+    Route::apiResource('/quizzes', App\Http\Controllers\Api\Quiz\QuizController::class, ['only' => ['index', 'show'], 'as' => 'api/quizzes']);
+    Route::apiResource('/takes', App\Http\Controllers\Api\Quiz\TakeController::class, ['only' => ['index', 'show'], 'as' => 'api/takes']);
     Route::post('/logout', [App\Http\Controllers\Api\Auth\LogoutController::class, 'logout']);
     Route::get('/quizzes/{quiz_id}/start', [App\Http\Controllers\Api\Quiz\TakeController::class, 'start']);
     Route::post('/quizzes/{quiz_id}/finish', [App\Http\Controllers\Api\Quiz\TakeController::class, 'finish']);
